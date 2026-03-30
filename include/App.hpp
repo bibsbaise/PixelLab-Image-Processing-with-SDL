fe59bb2e088fe7b7c70c9cb1834e66d3e53d5628
Daniel Omiya <daniel.omiya@grupoboticario.com.br>
2026-03-30 10:38:40 -0300

    refactor App class to improve initialization and add image processing features

--- include/App.hpp
+++ include/App.hpp
@@ -1,8 +1,35 @@
 #pragma once
 
 #include "Button.hpp"
+#include "Histogram.hpp"
 #include "SDLDeleter.hpp"
 #include <memory>
+#include <string>
+
+// dimensão das janelas
+constexpr int kMaxWindowSize = 720;
+constexpr int kToolsWindowWidth = 480;
+constexpr int kToolsWindowHeight = 360;
+
+// cor de fundo das janelas
+constexpr SDL_Color kWindowBgColor{240, 242, 245, 255}; // #f0f2f5
+
+// config do butão
+constexpr SDL_Color kButtonTextColor{255, 255, 255, 255};   // #FFFFFF
+constexpr SDL_Color kButtonBgColor{0, 123, 255, 255};       // #007bff
+constexpr SDL_Color kButtonHoverBgColor{51, 149, 255, 255}; // #3395FF
+constexpr SDL_Color kButtonPressBgColor{0, 86, 179, 255};   // #0056B3
+
+// config do histograma
+constexpr SDL_Color kHistogramBarColor{102, 102, 102, 255}; // #666666
+constexpr SDL_Color kHistogramOutlineColor{0, 0, 0, 255};   // #000000
+
+// textos
+constexpr char kWindowDefaultTitle[] = "PixelLab - Grayscale";
+constexpr char kWindowEqualizedTitle[] = "PixelLab - Equalized";
+constexpr char kToolsWindowTitle[] = "PixelLab - Tools";
+constexpr char kEqualizeText[] = "Equalizar";
+constexpr char kEqualizeOffText[] = "Ver original";
 
 class App {
 public:
@@ -15,20 +42,37 @@ public:
   App(App &&) = delete;
   App &operator=(App &&) = delete;
 
-  bool init();
+  bool init(const std::string &imagePath);
   void run();
 
 private:
+  bool loadImage(const std::string &imagePath);
+  bool createTextureFromCurrent();
+  void toggleEqualization();
   void handleEvents(bool &running);
   void handleKeyDown(const SDL_KeyboardEvent &e, bool &running);
   void render();
+  void positionWindows();
 
 private:
-  WindowPtr window_;
-  RendererPtr renderer_;
+  // monte de ponteiros inteligentes para gerenciar recursos do SDL
+  WindowPtr mainWindow_;
+  WindowPtr toolsWindow_;
+  RendererPtr mainRenderer_;
+  RendererPtr toolsRenderer_;
   TextEnginePtr textEngine_;
   TexturePtr textTexture_;
   FontPtr font_;
+  SurfacePtr originalGraySurface_;
+  SurfacePtr equalizedSurface_;
+  TexturePtr imageTexture_;
 
-  std::unique_ptr<Button> btnClickMe_;
+  // equalização
+  bool isEqualized_ = false;
+  std::array<int, 256> histogramGrayscaleData_{};
+  std::array<int, 256> histogramEqualizedData_{};
+
+  // componentes da interface
+  std::unique_ptr<Button> btnToggleEqualization_;
+  std::unique_ptr<Histogram> histogram_;
 };
--- src/App.cpp
+++ src/App.cpp
@@ -1,19 +1,27 @@
 #include "App.hpp"
+#include "ImageProcessing.hpp"
+
+#include <SDL3_image/SDL_image.h>
 
 App::App() = default;
 
 App::~App() {
+  btnToggleEqualization_.reset();
+  imageTexture_.reset();
+  originalGraySurface_.reset();
+  equalizedSurface_.reset();
   font_.reset();
   textTexture_.reset();
-  renderer_.reset();
-  window_.reset();
-  btnClickMe_.reset();
+  mainRenderer_.reset();
+  mainWindow_.reset();
+
+  toolsWindow_.reset();
 
   TTF_Quit();
   SDL_Quit();
 }
 
-bool App::init() {
+bool App::init(const std::string &imagePath) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
     return false;
@@ -25,18 +33,44 @@ bool App::init() {
     return false;
   }
 
-  SDL_Window *rawWindow = nullptr;
-  SDL_Renderer *rawRenderer = nullptr;
+  loadImage(imagePath);
 
-  if (!SDL_CreateWindowAndRenderer("PixelLab", 800, 600, 0, &rawWindow, &rawRenderer)) {
+  SDL_Window *rawMainWindow = nullptr;
+  SDL_Renderer *rawMainRenderer = nullptr;
+
+  int mainWindowWidth = originalGraySurface_->w, mainWindowHeight = originalGraySurface_->h;
+  if (mainWindowWidth > kMaxWindowSize || mainWindowHeight > kMaxWindowSize) {
+    float aspectRatio = static_cast<float>(kMaxWindowSize) / std::max(mainWindowWidth, mainWindowHeight);
+    mainWindowWidth = static_cast<int>(mainWindowWidth * aspectRatio);
+    mainWindowHeight = static_cast<int>(mainWindowHeight * aspectRatio);
+    SDL_Log("Redimensionando imagem/janela para caber na tela: %dx%d", mainWindowWidth, mainWindowHeight);
+  }
+
+  if (!SDL_CreateWindowAndRenderer(kWindowDefaultTitle, mainWindowWidth, mainWindowHeight,
+                                   SDL_WINDOW_INPUT_FOCUS, &rawMainWindow, &rawMainRenderer)) {
     SDL_Log("Failed to create window and renderer: %s", SDL_GetError());
     TTF_Quit();
     SDL_Quit();
     return false;
   }
 
-  window_.reset(rawWindow);
-  renderer_.reset(rawRenderer);
+  SDL_Window *rawToolsWindow;
+  SDL_Renderer *rawToolsRenderer;
+
+  if (!SDL_CreateWindowAndRenderer(kToolsWindowTitle, kToolsWindowWidth, kToolsWindowHeight,
+                                   SDL_WINDOW_UTILITY, &rawToolsWindow, &rawToolsRenderer)) {
+    SDL_Log("Failed to create tools window and renderer: %s", SDL_GetError());
+    SDL_DestroyRenderer(rawMainRenderer);
+    SDL_DestroyWindow(rawMainWindow);
+    TTF_Quit();
+    SDL_Quit();
+    return false;
+  }
+
+  mainWindow_.reset(rawMainWindow);
+  mainRenderer_.reset(rawMainRenderer);
+  toolsWindow_.reset(rawToolsWindow);
+  toolsRenderer_.reset(rawToolsRenderer);
 
   const char *fontPath = SDL_getenv("APP_FONT_PATH");
   if (!fontPath || !*fontPath) {
@@ -48,8 +82,10 @@ bool App::init() {
     fontPath = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf";
 #else
     SDL_Log("No default font path for this platform. Please set APP_FONT_PATH environment variable.");
-    renderer_.reset();
-    window_.reset();
+    mainRenderer_.reset();
+    mainWindow_.reset();
+    toolsWindow_.reset();
+    toolsRenderer_.reset();
     TTF_Quit();
     SDL_Quit();
     return false;
@@ -59,30 +95,96 @@ bool App::init() {
   font_.reset(TTF_OpenFont(fontPath, 24));
   if (!font_) {
     SDL_Log("Failed to load font: %s", SDL_GetError());
-    renderer_.reset();
-    window_.reset();
+    mainRenderer_.reset();
+    mainWindow_.reset();
+    toolsWindow_.reset();
+    toolsRenderer_.reset();
     TTF_Quit();
     SDL_Quit();
     return false;
   }
 
-  textEngine_.reset(TTF_CreateRendererTextEngine(rawRenderer));
+  textEngine_.reset(TTF_CreateRendererTextEngine(rawToolsRenderer));
   if (!textEngine_) {
     SDL_Log("Failed to create text engine: %s", SDL_GetError());
     font_.reset();
-    renderer_.reset();
-    window_.reset();
+    mainRenderer_.reset();
+    mainWindow_.reset();
+    toolsWindow_.reset();
+    toolsRenderer_.reset();
     TTF_Quit();
     SDL_Quit();
     return false;
   }
 
-  btnClickMe_.reset(new Button(renderer_.get(), textEngine_.get(), font_.get(), "Clica eu",
-                               SDL_FRect{300, 200, 200, 50}, SDL_Color{100, 149, 237, 255} /* #6495ed */,
-                               SDL_Color{255, 255, 255, 255} /* #fff */,
-                               []() { SDL_Log("Button clicked!"); }));
+  createTextureFromCurrent();
+  histogram_ =
+      std::make_unique<Histogram>(rawToolsRenderer, histogramGrayscaleData_, SDL_FRect{20, 20, 440, 240},
+                                  kHistogramBarColor, kHistogramOutlineColor);
+  btnToggleEqualization_ =
+      std::make_unique<Button>(rawToolsRenderer, textEngine_.get(), font_.get(), kEqualizeText,
+                               SDL_FRect{240, 280, 220, 50}, kButtonTextColor, kButtonBgColor,
+                               kButtonHoverBgColor, kButtonPressBgColor, [this]() { toggleEqualization(); });
+
+  positionWindows();
 
   return true;
+}
+
+bool App::loadImage(const std::string &imagePath) {
+  SurfacePtr loadedSurface(IMG_Load(imagePath.c_str()));
+
+  if (!loadedSurface) {
+    SDL_Log("Failed to load image '%s': %s", imagePath.c_str(), SDL_GetError());
+    return false;
+  }
+
+  originalGraySurface_ = convertToGrayscale(loadedSurface);
+  if (!originalGraySurface_) {
+    SDL_Log("Failed to convert image to grayscale");
+    return false;
+  }
+
+  histogramGrayscaleData_ = calculateHistogram(originalGraySurface_.get());
+
+  return true;
+}
+
+bool App::createTextureFromCurrent() {
+  SDL_Surface *currentSurface = isEqualized_ ? equalizedSurface_.get() : originalGraySurface_.get();
+
+  imageTexture_.reset(SDL_CreateTextureFromSurface(mainRenderer_.get(), currentSurface));
+  if (!imageTexture_) {
+    SDL_Log("Failed to create image texture: %s", SDL_GetError());
+    return false;
+  }
+
+  return true;
+}
+
+void App::toggleEqualization() {
+  if (!originalGraySurface_) {
+    return;
+  }
+
+  if (!equalizedSurface_) {
+    equalizedSurface_ = equalizeHistogram(originalGraySurface_);
+    histogramEqualizedData_ = calculateHistogram(equalizedSurface_.get());
+  }
+
+  if (!isEqualized_) {
+    isEqualized_ = true;
+    SDL_SetWindowTitle(mainWindow_.get(), kWindowEqualizedTitle);
+    histogram_->setData(histogramEqualizedData_);
+    btnToggleEqualization_->setText(kEqualizeOffText);
+  } else {
+    isEqualized_ = false;
+    SDL_SetWindowTitle(mainWindow_.get(), kWindowDefaultTitle);
+    histogram_->setData(histogramGrayscaleData_);
+    btnToggleEqualization_->setText(kEqualizeText);
+  }
+
+  createTextureFromCurrent();
 }
 
 void App::run() {
@@ -97,10 +199,6 @@ void App::run() {
 void App::handleEvents(bool &running) {
   SDL_Event e{};
 
-  // trocar a desgraça do SDL_PollEvent pelo SDL_WaitEvent
-  // fez o SDL parar de esquentar o mac com o app idle
-  // SDL_PollEvent -> jogos ou app com interacao pesada
-  // SDL_WaitEvent -> ui q pode esperar por input do usuario
   if (SDL_WaitEvent(&e)) {
     switch (e.type) {
     case SDL_EVENT_QUIT:
@@ -112,12 +210,16 @@ void App::handleEvents(bool &running) {
     default:
       break;
     }
-    btnClickMe_->handleEvent(e);
+
+    btnToggleEqualization_->handleEvent(e);
   }
 }
 
 void App::handleKeyDown(const SDL_KeyboardEvent &e, bool &running) {
   switch (e.key) {
+  case SDLK_E:
+    toggleEqualization();
+    break;
   case SDLK_S:
     SDL_Log("Key 'S' pressed but save logic is not implemented yet");
     // TODO: implementar logica de salvar imagem processada
@@ -132,11 +234,43 @@ void App::handleKeyDown(const SDL_KeyboardEvent &e, bool &running) {
 }
 
 void App::render() {
-  const SDL_Color bg = {204, 204, 204, 255}; // #ccc
-  SDL_SetRenderDrawColor(renderer_.get(), bg.r, bg.g, bg.b, bg.a);
-  SDL_RenderClear(renderer_.get());
+  // limpa com a cor de fundo antes de renderizar
+  SDL_SetRenderDrawColor(mainRenderer_.get(), kWindowBgColor.r, kWindowBgColor.g, kWindowBgColor.b,
+                         kWindowBgColor.a);
+  SDL_RenderClear(mainRenderer_.get());
+  SDL_SetRenderDrawColor(toolsRenderer_.get(), kWindowBgColor.r, kWindowBgColor.g, kWindowBgColor.b,
+                         kWindowBgColor.a);
+  SDL_RenderClear(toolsRenderer_.get());
 
-  btnClickMe_->render(renderer_.get());
+  // renderiza a imagem
+  if (imageTexture_) {
+    SDL_RenderTexture(mainRenderer_.get(), imageTexture_.get(), nullptr, nullptr);
+  }
 
-  SDL_RenderPresent(renderer_.get());
+  // renderiza os componentes
+  btnToggleEqualization_->render();
+  histogram_->render();
+
+  // apresenta tudo o que foi renderizado
+  SDL_RenderPresent(mainRenderer_.get());
+  SDL_RenderPresent(toolsRenderer_.get());
 };
+
+void App::positionWindows() {
+  if (!mainWindow_ || !toolsWindow_) {
+    SDL_Log("Cannot position windows, one or both of them are null");
+    return;
+  }
+  SDL_SetWindowPosition(mainWindow_.get(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+  int x, y, width, height;
+  SDL_GetWindowSize(mainWindow_.get(), &width, &height);
+  SDL_GetWindowPosition(mainWindow_.get(), &x, &y);
+
+  int toolsX = x + width, toolsY = y + (height - kToolsWindowHeight) / 2;
+  SDL_SetWindowPosition(toolsWindow_.get(), toolsX, toolsY);
+
+  // traz pra frente as janelas
+  SDL_RaiseWindow(mainWindow_.get());
+  SDL_RaiseWindow(toolsWindow_.get());
+}
